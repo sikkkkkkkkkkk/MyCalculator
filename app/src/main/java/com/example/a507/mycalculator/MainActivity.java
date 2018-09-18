0a6d65bfda9dd12d9efac4350e45eff2b99567cc
namsik <wa6979@naver.com>
2018-09-18 16:52:06 +0900

    new interface

--- app/src/main/java/com/example/a507/mycalculator/MainActivity.java
+++ app/src/main/java/com/example/a507/mycalculator/MainActivity.java
@@ -2,12 +2,36 @@ package com.example.a507.mycalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    protected double ans = 0.;  //answer
+    protected Button;
+
+    protected void inputNum(){
+
+    }
+
+    protected double op4(double){
+        return 0.;
+    }
+
+    protected double (double){
+        return 0.;
+    }
+
+    void printNum(double){
+
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btEqual = (Button) findViewById(R.id.btnEqual);
+        btEqual
     }
 }
